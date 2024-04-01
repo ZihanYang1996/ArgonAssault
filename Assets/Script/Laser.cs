8e74c235ff2b3cdd4af7c5345a65ca8095f6697c
Zihan Yang <ws704603301@gmail.com>
2024-04-01 14:46:11 -0400

    Now, the laser is firing towards the mouse

--- Assets/Script/Laser.cs
+++ Assets/Script/Laser.cs
@@ -7,15 +7,33 @@ public class Laser : MonoBehaviour
     
     // List<ParticleSystem> lasers = new List<ParticleSystem>();
     ParticleSystem[] lasers = new ParticleSystem[2];
+    Transform[] laserTransforms = new Transform[2];
+    Vector3 mousePosition;
+    [SerializeField] float laserDistance = 100f;
 
     void Awake()
     {
         ParticleSystem leftLaser = gameObject.transform.Find("LaserLeft").GetComponent<ParticleSystem>();
         ParticleSystem rightLaser = gameObject.transform.Find("LaserRight").GetComponent<ParticleSystem>();
+
+        laserTransforms[0] = leftLaser.gameObject.transform;
+        laserTransforms[1] = rightLaser.gameObject.transform;
+
         // lasers.Add(leftLaser);
         // lasers.Add(rightLaser);
         lasers[0] = leftLaser;
         lasers[1] = rightLaser;
+    }
+
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition.z = laserDistance;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        foreach (Transform laserTransform in laserTransforms)
+        {
+            laserTransform.LookAt(mouseWorldPosition);
+        }
     }
 
     public void Fire()
