12621413b88c0c8896179475eb27e9bece1697cc
Zihan Yang <ws704603301@gmail.com>
2024-04-01 16:19:16 -0400

    Add collision to particle system, and destroy enemy when it's hit by laser

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    void OnParticleCollision(GameObject other)
+    {
+        // Debug.Log("Enemy hit by laser");
+        Destroy(gameObject);
+    }
+}
