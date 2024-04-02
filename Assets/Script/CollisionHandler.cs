8bb2097a443341dc475ee539ce835b6252488b93
Zihan Yang <ws704603301@gmail.com>
2024-04-02 13:29:08 -0400

    Add explosion

--- Assets/Script/CollisionHandler.cs
+++ Assets/Script/CollisionHandler.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    [SerializeField] GameObject explosion;
+
+    void OnCollisionEnter(Collision other)
     {
-        Debug.Log("This is " + gameObject.name + " and it collided with " + other.gameObject.name);
+        Explode(other.contacts[0].point);
+        Invoke("ReloadScene", 1f);
+        // ReloadScene();
+    }
+
+    void Explode(Vector3 location)
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        GameObject explosionInstance = Instantiate(explosion, location, Quaternion.identity);
+        ParticleSystem explosionParticleSystem = explosionInstance.GetComponent<ParticleSystem>();
+        Destroy(explosionInstance, explosionParticleSystem.main.duration + explosionParticleSystem.main.startLifetime.constantMax);
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject explosion;
     void OnParticleCollision(GameObject other)
     {
+        List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+        other.GetComponent<ParticleSystem>().GetCollisionEvents(gameObject, collisionEvents);
+        Explode(collisionEvents[0].intersection);
         // Debug.Log("Enemy hit by laser");
         Destroy(gameObject);
+    }
+
+    void Explode(Vector3 location)
+    {
+        GameObject explosionInstance = Instantiate(explosion, location, Quaternion.identity);
+        ParticleSystem explosionParticleSystem = explosionInstance.GetComponent<ParticleSystem>();
+        Destroy(explosionInstance, explosionParticleSystem.main.duration + explosionParticleSystem.main.startLifetime.constantMax);
     }
 }
